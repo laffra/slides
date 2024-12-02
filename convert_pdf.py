6fad2a6eb60786f507f15e414c8aafcc5dd09117
Chris Laffra <laffra@gmail.com>
2024-12-02 13:17:14 +0100

    Download slides from URL

--- convert_pdf.py
+++ convert_pdf.py
@@ -2,15 +2,50 @@
 LTK player written in Python for (Google Slides) PDF
 """
 
+import os
+import re
+import shutil
+import urllib.request
+
 import pymupdf
 
-print("Loading PDF...")
-doc = pymupdf.open('slides.pdf')
-for n in range(doc.page_count):
-    print(f"Processing page {n + 1}")
-    page = doc.load_page(n)
-    pixmap = page.get_pixmap(dpi=300)
-    img = pixmap.tobytes()
-    with open(f"slides/page-{n+1}.png", "wb") as f:
-        f.write(img)
-    print(f"Saving page Output_{n + 1}.png => {len(img)} bytes")
+PDF_FILENAME = "slides.pdf"
+
+
+def load_pdf(filename):
+    """ Load a PDF from a URL """
+    url = input("Enter your Google Slides URL: ")
+    url = re.sub("/edit.*", "/export/pdf", url)
+    print("Loading ", url)
+    with open(filename, "bw") as fp:
+        data = urllib.request.urlopen(url).read()
+        fp.write(data)
+        print(f"Saved {len(data):,} bytes into {filename}")
+
+
+def generate_slides(filename):
+    """ Load the contents of a PDF and generate slides. """
+    doc = pymupdf.open(filename)
+    total_size = 0
+    for n in range(doc.page_count):
+        page = doc.load_page(n)
+        pixmap = page.get_pixmap(dpi=300)
+        img = pixmap.tobytes()
+        with open(f"slides/page-{n+1}.png", "wb") as f:
+            f.write(img)
+        print(f"Saving page Output_{n + 1}.png => {len(img):,} bytes")
+        total_size += len(img)
+    return total_size
+
+
+def clear():
+    """ Remove the old slides """
+    shutil.rmtree('slides')
+    os.mkdir('slides')
+
+
+
+clear()
+load_pdf(PDF_FILENAME)
+print(f"Total size is {generate_slides(PDF_FILENAME):,} bytes")
+os.remove(PDF_FILENAME)
